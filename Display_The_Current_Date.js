439cfe49bcf12010b6c13b012812d3e1557b7ef2
Laveena-2806 <51774037+Laveena-2806@users.noreply.github.com>
2020-11-20 16:09:02 +0530

    Displaying the current date
    Displaying the current date with format MM/DD/Year.

---
+++ Display_The_Current_Date.js
@@ -0,0 +1,4 @@
+// Program to display current date 
+var date = new Date();
+var todayDate = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+console.log("Today's date is: " + todayDate);
