297dd83d53c3f40abcedff5cd81ccfa812dc1a15
Laveena-2806 <51774037+Laveena-2806@users.noreply.github.com>
2020-11-25 15:59:32 +0530

    Update Convert_given_number_in_Hours_and_Minutes.js

--- Convert_given_number_in_Hours_and_Minutes.js
+++ Convert_given_number_in_Hours_and_Minutes.js
@@ -3,9 +3,9 @@ var readLineSync = require('readline-sync');
 
 function numberToHoursAndMinutes() {
     var number = readLineSync.question("Enter the number:");
-    const ONEMINUTE = 60;
-    var hours = Math.floor(number / ONEMINUTE);
-    var minutes = number % ONEMINUTE;
+    const MINUTEINONEHOUR = 60;
+    var hours = Math.floor(number / MINUTEINONEHOUR);
+    var minutes = number % MINUTEINONEHOUR;
     console.log("The conversion of " + number + " into hours and minutes is:" + hours + "hours " + minutes + "minutes.");
 }
 numberToHoursAndMinutes();
