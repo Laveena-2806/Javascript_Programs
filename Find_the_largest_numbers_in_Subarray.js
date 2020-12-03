1d014b6877130708d36e71eb333a1a6f2c0eed8e
Laveena-2806 <51774037+Laveena-2806@users.noreply.github.com>
2020-12-03 19:52:24 +0530

    Find the largest numbers from subarray.
    Program to find the largest number from each subarray of an array.

--- Find_the_largest_numbers_in_Subarray.js
+++ Find_the_largest_numbers_in_Subarray.js
@@ -3,7 +3,6 @@ var readLineSync = require('readline-sync');
 
 function largestArrayFromSubarray() {
     let mainArray = [];
-    let largest = 0;
     let largestArray = [];
     let limitOfMainArray = readLineSync.question("Enter the limit of main array:");
     limitOfMainArray = parseInt(limitOfMainArray);
@@ -17,19 +16,22 @@ function largestArrayFromSubarray() {
             arr[j] = elementsToPush;
         }
         mainArray[i] = arr;
-
-
-    }
-    for (let i = 0; i <= mainArray[i]; i++) {
-        for (let j = 0; j < mainArray[j].length; j++) {
-            if (mainArray[i][j] > largest) {
-                largest = mainArray[i][j];
+        for (var n = 0; n < mainArray.length; n++) {
+            var largestNumber = mainArray[n][0];
+            for (var sb = 1; sb < mainArray[n].length; sb++) {
+                if (mainArray[n][sb] > largestNumber) {
+                    largestNumber = mainArray[n][sb];
+                }
             }
-            console.log(largest);
-            largestArray.push(largest);
+
+            largestArray[n] = largestNumber;
         }
+
     }
+
+    console.log("The Array with subarray entered is:");
     console.log(mainArray);
+    console.log("The array after getting the largest number from is:")
     console.log(largestArray);
 
 }
