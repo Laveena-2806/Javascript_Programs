fea788888f5469d245f10672232a6be31b0e02fa
Laveena-2806 <51774037+Laveena-2806@users.noreply.github.com>
2020-12-07 16:20:38 +0530

    Sum of primes numbers.
    Program to return the sum of all prime numbers that are less than or equal to the entered number.

---
+++ Sum_Of_Prime_till_Range.js
@@ -0,0 +1,25 @@
+// sum of all the prime number till the specified range
+var readLineSync = require("readline-sync");
+(function sumOfPrime() {
+
+    let sum = 0;
+    let num = readLineSync.question("Enter the number of primes that you want to add:")
+    num = parseInt(num);
+
+    function checkPrime(i) {
+        for (var k = 2; k < i; k++) {
+            if (i % k === 0) {
+                return;
+            }
+        }
+        return true;
+    }
+    for (var i = 2; i <= num; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    console.log(`The sum of ${num} prime numbers is:${sum}.`);
+    // returns 17 - the correct answer. 
+})
+();
