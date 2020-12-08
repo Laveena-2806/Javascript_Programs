8824172903ceede65256b408f0d33fbce9ea1b73
Laveena-2806 <51774037+Laveena-2806@users.noreply.github.com>
2020-12-08 17:21:09 +0530

    Binary to string conversion.
    Program to convert the entered binary code into a readable format.

---
+++ Binary_to_String.js
@@ -0,0 +1,16 @@
+// program to convert the binary code into readable format.
+// example ('01000001 01110010 01100101 01101110 00100111 01110100') to Aren't.
+var readLineSync = require('readline-sync');
+const binaryToString = () => {
+    let binaryForm = readLineSync.question("Enter the sentence in binary form means in 0 and 1 format:");
+    let newBinary = binaryForm.split(" ");
+    let convertedToString = [];
+
+    for (i = 0; i < newBinary.length; i++) {
+        convertedToString.push(String.fromCharCode(parseInt(newBinary[i], 2)));
+    }
+    let newString = convertedToString.join("");
+    console.log(newString);
+
+}
+binaryToString();
