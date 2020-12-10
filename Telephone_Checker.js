5568d55bb47ee072ed9ef4dc8a96335bfee4607b
Laveena-2806 <51774037+Laveena-2806@users.noreply.github.com>
2020-12-10 16:29:42 +0530

    Telephone number check
    Program to validate the telephone number entered by a user using regex.

--- Telephone_Checker.js
+++ Telephone_Checker.js
@@ -3,7 +3,7 @@ var readLineSync = require('readline-sync');
 (function telephoneChecker() {
     let telephoneNumber = readLineSync.question("Enter the telephone number for validating:")
     telephoneNumber = parseInt(telephoneNumber);
-    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
     console.log(regex.test(telephoneNumber));
 
 })
